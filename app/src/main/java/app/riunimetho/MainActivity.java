75ae16475c6ea219501a63bba27413e3a951e041
bensalcie <bensalcie@gmail.com>
2021-12-21 14:18:59 +0300

    Added about us page

--- app/src/main/java/app/riunimetho/MainActivity.java
+++ app/src/main/java/app/riunimetho/MainActivity.java
@@ -1,12 +1,14 @@
 package app.riunimetho;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -41,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery,R.id.sportsFragment, R.id.nav_slideshow,R.id.masomoFragment,R.id.usafiriFragment, R.id.tourismFragment, R.id.youthsFragment, R.id.uchumi, R.id.mwangazaFragment, R.id.usafiriFragment, R.id.nav_home)
+                R.id.nav_home, R.id.nav_gallery,R.id.sportsFragment,R.id.aboutFragment, R.id.nav_slideshow,R.id.masomoFragment,R.id.usafiriFragment, R.id.tourismFragment, R.id.youthsFragment, R.id.uchumi, R.id.mwangazaFragment, R.id.usafiriFragment, R.id.nav_home)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
@@ -62,4 +64,17 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.aboutFragment){
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.aboutFragment);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
