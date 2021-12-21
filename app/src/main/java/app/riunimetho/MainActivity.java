6c55fb5b14fde4f4638e4e08f87da42dca38f02e
bensalcie <bensalcie@gmail.com>
2021-12-21 14:02:13 +0300

    FIxed page issues

--- app/src/main/java/app/riunimetho/MainActivity.java
+++ app/src/main/java/app/riunimetho/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_gallery,R.id.sportsFragment, R.id.nav_slideshow,R.id.masomoFragment,R.id.usafiriFragment, R.id.tourismFragment, R.id.youthsFragment, R.id.uchumi, R.id.mwangazaFragment, R.id.usafiriFragment, R.id.nav_home)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
